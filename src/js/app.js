437032eddc75a3f92742d883119230d2ee20a410
Enyel Liranzo <xardimodschannel@gmail.com>
2024-02-02 02:11:01 -0400

    [add] Adding JS and a mobile version

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     createImages();
+    mobileMenuSlide();
 });
 
 function createImages () {
@@ -18,11 +19,52 @@ function createImages () {
         `
 
         ulImage.appendChild(image);
-    }
+    }    
+}
+
+if (window.matchMedia("(min-width: 360px)").matches && window.matchMedia("(max-width: 768px)").matches ) {
+
+    function mobileMenuSlide () {
+        const mobileMenu = document.querySelector(".mobile-menu");
+        const mobileMenuById = document.getElementById("mobile-menu");
+        const body = document.body;
     
+        mobileMenu.addEventListener("click", () => {
+            const mobileSlide = document.getElementById("mobile-menu-slide");
+            if (mobileSlide.style.display === "none") {
+                mobileMenuById.src = "build/img/close-line.svg";
+                mobileSlide.style.display = "block";
+                body.style.overflow = "hidden";
+            } else {
+                mobileMenuById.src = "build/img/align-justify.svg";
+                mobileSlide.style.display = "none";
+                body.style.overflow = "auto";
+            }
+        });
 
+        // Obtén todos los enlaces del menú
+        const menuLinks = document.querySelectorAll(".mobile-menu-slide-link");
 
+        // Agrega un evento a cada enlace para cerrar el menú al hacer clic
+        menuLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                const mobileSlide = document.getElementById("mobile-menu-slide");
+                mobileMenuById.src = "build/img/align-justify.svg";
+                mobileSlide.style.display = "none";
+                body.style.overflow = "auto";
+            });
+        });
 
+        // const profileImage = document.querySelector(".profile-picture");
 
+        // profileImage.addEventListener("click", () => {
+        //     const mobileSlide = document.getElementById("mobile-menu-slide");
+        //     mobileMenuById.src = "build/img/align-justify.svg";
+        //     mobileSlide.style.display = "none";
+        //     body.style.overflow = "auto";
+        // });
+    }
+} else {
+    mobileSlide.style.display = "none";
 }
 
